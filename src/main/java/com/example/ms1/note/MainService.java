6e34c815c6156673b05924a81b64bc5849001a3c
tpdl9612 <tpdl9612@gmail.com>
2024-05-14 13:01:37 +0900

    moveTree modal layout

--- src/main/java/com/example/ms1/note/MainService.java
+++ src/main/java/com/example/ms1/note/MainService.java
@@ -19,7 +19,8 @@ public class MainService {
     private final NoteService noteService;
 
     public MainDataDto getDefaultMainData() {
-        List<Notebook> notebookList = notebookService.getNotebookList();
+//        List<Notebook> notebookList = notebookService.getNotebookList();
+        List<Notebook> notebookList = notebookService.getTopNotebookList();
 
         if (notebookList.isEmpty()) {
             Notebook notebook = this.saveDefaultNotebook();
